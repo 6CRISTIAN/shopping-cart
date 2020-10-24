dafd44d1ea5744c653a4af79fc6d1c2eaae75b3a
Cristian Gómez <cristian.gomez@zentagroup.com>
2020-10-24 09:47:44 -0300

    Init project

---
+++ app.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+    user: 'bsale_test',
+    password: 'bsale_test',
+    database: 'bsale_test'
+})
+
+connection.connect((err) => {
+    if (err) throw err
+    console.log('Connected!')
+})
